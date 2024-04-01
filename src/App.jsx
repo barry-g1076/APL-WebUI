9b252154acf1e94556290066b6e7859d7b26411d
Hugh Scott <48400324+HughScott2002@users.noreply.github.com>
2024-04-01 14:21:52 -0500

    Hid pricing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Pricing from "./components/Pricing";
+// import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
@@ -17,7 +17,7 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
-        <Pricing />
+        {/* <Pricing /> */}
         <Roadmap />
         <Footer />
       </div>
