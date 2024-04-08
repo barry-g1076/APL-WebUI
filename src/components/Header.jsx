2667956eac4f785f8e7242d6e401cde896582742
Barrington Patterson <barry545young@gmail.com>
2024-04-08 11:19:58 -0500

    implemented download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import document from "../constants/APL_Group_Project_Report.pdf";
 
 const Header = () => {
   const pathname = useLocation();
@@ -75,7 +76,10 @@ const Header = () => {
         </nav>
 
         <a
-          href="#Editor"
+          href={document}
+          download="TypeSnake-Documentation"
+          target="_blank"
+          rel="noreferrer"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
           Documentation
