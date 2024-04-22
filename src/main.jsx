cf8f099a78e78d1fc49c9ef9b8d93782dfecc807
Hugh Scott <48400324+HughScott2002@users.noreply.github.com>
2024-04-22 01:49:24 -0500

    Vercel Analytics Added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
+      <Analytics />
     </Router>
   </React.StrictMode>
 );
