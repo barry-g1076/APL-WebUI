ec673f875a851f0a4ba2584a5aca07851d771bf4
Hugh Scott <48400324+HughScott2002@users.noreply.github.com>
2024-05-15 17:46:11 -0500

    Commenting Out Analytics
    This is needed so the forking doesn't brick the build on in vercel.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 //Added Vercel Analytics
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
-      <Analytics />
+      {/* <Analytics /> */}
     </Router>
   </React.StrictMode>
 );
