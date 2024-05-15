1e2c560f5381bd51584c7a0974034f0f3cc6bdce
Hugh Scott <48400324+HughScott2002@users.noreply.github.com>
2024-05-15 17:31:27 -0500

    Update Editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -128,7 +128,7 @@ const CodeEditor = () => {
   };
 
   const generate_code = async () => {
-    await fetch("https://typesnake.azurewebsites.net/generate_code", {
+    await fetch("https://typesnakeapi-zmwrhpfjma-uc.a.run.app/generate_code", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
